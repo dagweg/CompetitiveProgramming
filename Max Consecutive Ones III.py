1434ba9cc0aec4a1d4587ef1ce2263920b2e7156
dagweg <90281138+dagweg@users.noreply.github.com>
2023-02-11 17:01:15 +0300

    Create Max Consecutive Ones III.py

---
+++ Max Consecutive Ones III.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+        right = 0
+        zero_count = 0
+        max_len = 0
+        
+        while right < len(nums):
+            if nums[right] == 0:
+                zero_count += 1
+            while zero_count > k:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+            max_len = max(max_len, right - left + 1)
+            right += 1
+        
+        return max_len
