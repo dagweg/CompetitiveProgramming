6b8135ed31a5d3d3264ad26173f8102dacf39076
dagweg <90281138+dagweg@users.noreply.github.com>
2023-02-14 14:59:38 +0300

    Update Range Sum Query 2D - Immutable.py

--- Range Sum Query 2D - Immutable.py
+++ Range Sum Query 2D - Immutable.py
@@ -19,12 +19,9 @@ class NumMatrix:
                 self.ps[i][j] = self.ps[i][j-1] + matrix[i][j]
 
 
-        for m in self.ps:
-            print(m)
-
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        
         sum = 0
-
         for i in range(row1, row2+1):
             if col1 - 1 < 0:
                 sum += self.ps[i][col2]
